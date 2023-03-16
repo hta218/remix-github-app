668cceba9b879910625aa2f34888a015a13d46b7
Leo Huynh <huynhtuananh21895@gmail.com>
2023-03-16 08:40:15 +0700

    feat: add tailwind, use future config

---
+++ app/routes/_index.tsx
@@ -0,0 +1,16 @@
+export default function Index() {
+  return (
+    <main className="p-16">
+      <h1 className="text-5xl font-medium mb-8">Weaverse github test</h1>
+      <div className="space-y-6">
+        {/* <div>App name: {data.name}</div> */}
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-medium py-2 px-4 rounded"
+          // onClick={handleInstallApp}
+        >
+          Install Github App
+        </button>
+      </div>
+    </main>
+  );
+}
